fb92e975ca6ce671f9db4238fb936a99b58ef847
kaspermunch <kaspermunch@birc.au.dk>
2026-04-24 11:45:08 +0200

    Update pd-lfs dependency and enhance logging in data processing scripts

--- build_parquet.py
+++ build_parquet.py
@@ -1,24 +1,21 @@
+import sys
 import pandas as pd
 from pd_lfs import read_parquet, write_parquet
 
 df_list = []
 for n in range(1, 23):
+    print(f"Chromosome {n}", file=sys.stderr)
     chrom = f'chr{n}'
     file_name = f'./steps/atlas.chr{n}.csv'
-    print(file_name)
     df = pd.read_csv(file_name, sep=r'\s*,\s*', skiprows=3, engine='python')
-    print(df.columns)
-    assert 0
     df['chrom'] = f'chr{n}'
     df['pos'] = df['Position']
     df.drop(columns=['Chromosome', 'Position'])
     df_list.append(df)
 df = pd.concat(df_list)    
 
-write_parquet(df, "results/atlas_variant_age.parquet", group="chrom", 
+column_subset = ['VariantID', 'Chromosome', 'Position', 'AlleleRef', 'AlleleAlt', 'AlleleAnc', 'AgeMode_Mut', 'AgeMean_Mut', 'AgeMedian_Mut', 'AgeCI95Lower_Mut', 'AgeCI95Upper_Mut']
+
+write_parquet(df[column_subset], "results/atlas_variant_age.parquet", group="chrom", 
               compression='gzip' # better but slower compression than zstd
               )
-
-# # HTTPS (after uploading the dir to a static host — GitHub Pages, S3 website, nginx, ...)
-# df = read_parquet("https://github.com/munch-group/atlas-variant-age/")
-
--- download.sh
+++ download.sh
@@ -3,6 +3,7 @@
 mkdir -p ./steps
 
 for N in {1..22}; do
+    echo "Chromosome $N"
     curl https://human.genome.dating/bulk/atlas.chr${N}.csv.gz | gzip -d > ./steps/atlas.chr${N}.csv
 done
 
