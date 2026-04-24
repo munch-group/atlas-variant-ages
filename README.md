085f34ff0e6b7b48164f998da04487f6f79afc56
kaspermunch <kaspermunch@birc.au.dk>
2026-04-24 12:30:43 +0200

    Update README and scripts for parquet file generation; enhance logging and update pd-lfs dependency

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
-Build parquet file:
+Download the publicly available csv files from https://human.genome.dating/download/index and build parquet data set with only the main columns (renamed to shorter lower case names):
 
-    ./download.sh && pixi run python build_parquet.py
+    ./download.sh && srun --mem-per-cpu=36g --time=03:00:00 --account=xy-drive --pty pixi run python build_parquet.py
 
+ 
--- build_parquet.py
+++ build_parquet.py
@@ -1,10 +1,21 @@
 import sys
 import pandas as pd
-from pd_lfs import read_parquet, write_parquet
+import gc
+import logging
+from pathlib import Path
+from pd_lfs import write_parquet
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    # format="%(asctime)s | %(levelname)-8s | %(message)s",
+    handlers=[logging.StreamHandler(), logging.FileHandler(Path(__file__).with_suffix('.log'))],
+)
+logger = logging.getLogger(Path(__file__).stem)
+logger.setLevel(logging.DEBUG)
 
 df_list = []
 for n in range(1, 23):
-    print(f"Chromosome {n}", file=sys.stderr)
+    logger.info(f"Reading chrom {n}")
     chrom = f'chr{n}'
     file_name = f'./steps/atlas.chr{n}.csv'
     df = pd.read_csv(file_name, sep=r'\s*,\s*', skiprows=3, engine='python')
@@ -12,10 +23,24 @@ for n in range(1, 23):
     df['pos'] = df['Position']
     df.drop(columns=['Chromosome', 'Position'])
     df_list.append(df)
-df = pd.concat(df_list)    
+
+logger.info(f"Concatenating data frames")
 
 column_subset = ['VariantID', 'Chromosome', 'Position', 'AlleleRef', 'AlleleAlt', 'AlleleAnc', 'AgeMode_Mut', 'AgeMean_Mut', 'AgeMedian_Mut', 'AgeCI95Lower_Mut', 'AgeCI95Upper_Mut']
+df = pd.concat(df_list)[column_subset].rename(columns={
+    'VariantID': 'variant_id', 'Chromosome': 'chrom', 'Position': 'pos', 
+    'AlleleRef': 'ref', 'AlleleAlt': 'alt', 'AlleleAnc': 'anc', 
+    'AgeMode_Mut': 'age_mode', 'AgeMean_Mut': 'age_mean', 'AgeMedian_Mut': 'age_median', 
+    'AgeCI95Lower_Mut': 'age_lo95ci', 'AgeCI95Upper_Mut': 'age_hi95ci'
+})
 
-write_parquet(df[column_subset], "results/atlas_variant_age.parquet", group="chrom", 
+logger.info(f"Garbage collecting")
+
+del df_list
+gc.collect()
+
+logger.info(f"Writing parquet")
+
+write_parquet(df, "results/atlas_variant_age.parquet", group="chrom", 
               compression='gzip' # better but slower compression than zstd
               )
